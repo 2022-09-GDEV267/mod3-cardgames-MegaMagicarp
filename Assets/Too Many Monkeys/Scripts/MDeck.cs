ef442fb713a45452b8ef550f564417b6391a5371
MegaMagicarp <66321765+MegaMagicarp@users.noreply.github.com>
2022-12-08 11:20:23 -0500

    building MDeck.cs

--- Assets/Too Many Monkeys/Scripts/MDeck.cs
+++ Assets/Too Many Monkeys/Scripts/MDeck.cs
@@ -1,19 +1,47 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
-public class MDeck : MonoBehaviour
+namespace Monkey
 {
-    // Start is called before the first frame update
-    void Start()
+    public class MDeck : MonoBehaviour
     {
-        
-    }
+        [Header("Set in Inspector")]
+        public Sprite[] faceSprites;
+        public Sprite cardBack;
+        public Sprite cardFront;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        // Prefabs
+        public GameObject prefabSprite;
+        public GameObject prefabCard;
+
+        [Header("Set Dynamically")]
+        public PT_XMLReader xmlr;
+        public List<string> cardNames;
+        public List<Card> cards;
+        public Transform deckAnchor;
+
+        public void InitDeck(string deckXMLText)
+        {
+            if (GameObject.Find("_Deck") == null)
+            {
+                GameObject anchorGO = new GameObject("_Deck");
+                deckAnchor = anchorGO.transform;
+            }
+
+            ReadDeck(deckXMLText);
+            MakeCards();
+        }
+
+        private void MakeCards()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void ReadDeck(string deckXMLText)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
